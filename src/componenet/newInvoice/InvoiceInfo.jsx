bc401b0c78bc95b6236d0be476b68f7d6bd04f25
badr ghanbi <bghanbi50@gmail.com>
2022-12-24 13:24:59 +0100

    add the basic styles for the invoiceinfo modal

--- src/componenet/newInvoice/InvoiceInfo.jsx
+++ src/componenet/newInvoice/InvoiceInfo.jsx
@@ -3,7 +3,7 @@ import './invoiceInfo.css'
 const InvoiceInfo = () => {
   return (
     <div className="overlay2">
-        <div className='absolute invoiceInfo top-20 text-white left-0 bottom-0 right-0   md:relative bg-white'>InvoiceInfo</div>
+        <div className='absolute invoiceInfo top-20 text-white bg-white left-0 bottom-0 right-0   md:relative  h-full '>InvoiceInfo</div>
     </div>
   )
 }
