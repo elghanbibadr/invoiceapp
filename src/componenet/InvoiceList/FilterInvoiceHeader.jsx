2bbf675b1af0a2a201d5850c7790be11026e3086
badr ghanbi <bghanbi50@gmail.com>
2022-12-24 13:47:24 +0100

    create the go back link for the invoiceinfo componenet

--- src/componenet/InvoiceList/FilterInvoiceHeader.jsx
+++ src/componenet/InvoiceList/FilterInvoiceHeader.jsx
@@ -4,7 +4,7 @@ import iconArrow from '../../assets/icon-arrow-down.svg'
 import FilterInvoiceSelectOption from './FilterInvoiceSelectOption'
 import Button from '../GlobalComponenet/Button'
 const FilterInvoiceHeader = () => {
-    const {invoiceNumber}=useContext(AppContext);
+    const {invoiceNumber,setInvoiceInfoMenuVisible}=useContext(AppContext);
     const [arrowRotated,setArrowRotated]=useState(false);
     const [isChoicesListVisible,setIsChoicesListVisible]=useState(false)
     const handleFilterArrowClicked=()=>{
@@ -12,11 +12,14 @@ const FilterInvoiceHeader = () => {
         setIsChoicesListVisible(!isChoicesListVisible)
     }
 
+    const handleClick=()=>{
+        setInvoiceInfoMenuVisible(true);
+    }
  return(
 <div className='filterInvoiceBox flex justify-between'>
      <div className="filterInvoicesDesc">
          <h1>Invoices</h1>
-         <p>  {invoiceNumber}   invoice</p>
+         <p>{invoiceNumber} invoice</p>
      </div>
      <div className="filerInvoiceChange flex relative items-center">
         <div className="filterInvoiceLabel flex items-center">
@@ -24,7 +27,7 @@ const FilterInvoiceHeader = () => {
             <img  onClick={handleFilterArrowClicked} className={`iconArrow m-2 ${arrowRotated ? 'rotate-180' : 'rotate-0'}`} src={iconArrow}/>
         </div>
        <FilterInvoiceSelectOption isVisible={isChoicesListVisible}/>
-       <Button btnWithIcon={true} id='bg-cyan' name='New'/>
+       <Button onClick={handleClick} btnWithIcon={true} id='bg-cyan' name='New'/>
     </div>
     </div>
  )
--- src/componenet/newInvoice/InvoiceInfo.jsx
+++ src/componenet/newInvoice/InvoiceInfo.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../Store/AppContext'
 import './invoiceInfo.css'
 const InvoiceInfo = () => {
+  const {setInvoiceInfoMenuVisible}=useContext(AppContext);
+  const handleOverlay2Clicked=(e)=>{
+    if (e.target.id==='overlay2'){
+      setInvoiceInfoMenuVisible(false);
+    }
+  }
+
+  const handleOnBackClick=(e)=>{
+    setInvoiceInfoMenuVisible(false);
+
+  }
   return (
-    <div className="overlay2">
-        <div className='absolute invoiceInfo top-20 text-white bg-white left-0 bottom-0 right-0   md:relative  h-full '>InvoiceInfo</div>
+    
+    <div id='overlay2' onClick={handleOverlay2Clicked} className="overlay2">
+        <div className='absolute invoiceInfo top-20 text-white bg-paleBlue left-0 bottom-0 right-0   md:relative  h-full '>
+        <div className='flex items-center md:hidden'>
+        <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg"><path d="M6.342.886L2.114 5.114l4.228 4.228" stroke="#9277FF" stroke-width="2" fill="none" fill-rule="evenodd" /></svg>
+        <a className='text-white cursor-pointer font-bold ml-3' onClick={handleOnBackClick}>Go Back</a>
+      </div>
+        </div>
     </div>
   )
 }
